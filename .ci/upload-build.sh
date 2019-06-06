05c3e156b1c38fbb0d9b1c8867f7ee50dc22dc25
Jamie Bliss <jamie@ivyleav.es>
2019-06-06 15:36:53 -0400

    Add some debugging

--- .ci/upload-build.sh
+++ .ci/upload-build.sh
@@ -23,6 +23,7 @@ for fpath in "${files_to_upload[@]}"; do
 	echo "Uploading '$fpath' to Github..."
 	name=$(basename "$fpath")
 	url_to_upload="https://uploads.github.com/repos/$CIRRUS_REPO_FULL_NAME/releases/$CIRRUS_RELEASE/assets?name=$name"
+	echo $url_to_upload
 	curl -X POST \
 		--data-binary @$fpath \
 		--header "Authorization: token $GITHUB_TOKEN" \
